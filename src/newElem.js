c86e6ee359d71415d4ab5f91017a3bf8f04212b2
Ivaw490 <div490@mail.ru>
2018-10-29 15:32:32 +0700

    3

---
+++ src/newElem.js
@@ -0,0 +1,22 @@
+import $ from 'jquery';
+
+// window.onload = () => {
+//   let elem = document.getElementsByClassName('circle');
+//   console.log(elem);
+//   elem.
+//   elem.classList.add('newclass');
+// }
+
+
+
+// $(document).ready = () => {
+//     console.log('hi');
+//     let elem = $('.circle');
+//     console.log(elem);
+    
+//     $(elem).addClass('.newClass');
+//     $(elem).on('click', () => {
+//         let data = new data();
+//         alert(data);
+//    })
+// }
