babc6a9470a55299a77def7d1bf257524fd8df15
Grant-grant <33813423+Grant-grant@users.noreply.github.com>
2018-10-31 21:53:13 +0300

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,18 +2,27 @@ import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 
 import Menu from './components/Menu';
+import SideMenu from './components/SideMenu';
 import Container from './components/Container';
+import Login from './components/Login';
+import Content from './components/Content';
+import ProjectInfo from './components/ProjectInfo';
+import ClickDate from './components/ClickDate';
 
 const items = [
-  { href: '/', title: 'Home' },
-  { href: '/news', title: 'News' },
-  { href: '/blog', title: 'Blog' },
+  { href: '/', title: 'Home' },  { href: '/news', title: 'News' },  { href: '/blog', title: 'Blog' },
 ];
 
 const items1 = [
-  { href: '/', title: 'Home1' },
-  { href: '/news', title: 'News1' },
-  { href: '/blog', title: 'Blog2' },
+  { href: '/', title: 'Home1' },  { href: '/news', title: 'News1' },  { href: '/blog', title: 'Blog2' },
+];
+
+const items2 = [
+  { href: '/', title: 'About' },  { href: '/special', title: 'Special' },  { href: '/contakcs', title: 'Contakcs' },
+];
+
+const fio = [
+  { family: 'Gamz',  name: 'Grant', secondfamily: 'Arkadievich'},  
 ];
 
 class App extends Component {
@@ -21,9 +30,17 @@ class App extends Component {
     return (
       <div className="box">
         <Container>
-          <Menu items={items} title="Main menu" />
-          Hello world!
-          <Menu items={items1} title="Just another menu" />
+          <div className="divcontent col-md-8" ><ProjectInfo items={fio}  /></div><div className="divcontent col-md-4" ><ClickDate  title="ClickDate"/></div>
+          <Menu items={items} title="Main menu" />		  
+		  <div className="contentall">
+		  <div className="divcontent col-md-8" ><Content /></div>
+		  <div className="divcontent col-md-4" >
+		  <Login  title="login"/>
+		  
+		  <SideMenu items={items2}  title="Side menu"/></div>
+		  </div>
+          <Menu items={items1} title="Footer menu" />
+		  
         </Container>
       </div>
     )
