f66d8433a12f5e9587c119693e45acebf6999199
UnNoTenshi <angelofsucsess@gmail.com>
2018-11-05 11:30:08 +0300

    Сделан 2 урок

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,47 @@
+'use strict';
+
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    main: path.resolve(__dirname, 'src', 'index.jsx'),
+    main: path.resolve(__dirname, 'src', 'index.js'),
   },
+
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
+
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
+        test: /\.js$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader'
         }
       },
+
       {
-        test: /\.css$/,
+        test: /\.scss$/,
         use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
-          use: ['css-loader'],
+          use: ['css-loader', 'sass-loader'],
         })
       }
     ]
   },
+
   plugins: [
-    new ExtractTextPlugin({ filename: 'style.css' }),
-    new HtmlWebpackPlugin({
+    new ExtractTextPlugin({
+      filename: 'style.css',
+    }),
+
+    new htmlWebpackPlugin({
       template: path.resolve(__dirname, 'src', 'index.html'),
-      filename: 'index.html',
-    })
+      filename: 'index.html'
+    }),
   ]
-}
+};
