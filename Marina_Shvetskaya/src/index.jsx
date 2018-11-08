d759d6b475d6d80711346129aee04fed65cf4301
missisol <missisololol@gmail.com>
2018-11-08 13:28:17 +0300

    index.jsx updated

--- Marina_Shvetskaya/src/index.jsx
+++ Marina_Shvetskaya/src/index.jsx
@@ -1,32 +1,35 @@
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css'; 
-import 'bootstrap/dist/js/bootstrap.bundle';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import data from './source/itemsMenu.jsx';
+import articles from './source/article';
+import categories from './source/categories';
+import archives from './source/archives';
 
 import Container from './components/Container';
 import Content from './components/Content';
 import Menu from './components/Menu';
-import Sidebar from './components/Sidebar';
-import Modal from './components/Modal';
+import Categories from './components/Categories';
+import Mymodal from './components/Modal';
 
 class Layout extends Component {
   
   render() {
     return (
       <Container>
-        <Menu items={data} title="Main menu" />
-        <Modal />
+        <Menu items = {data} titleBlog = "My SuperBlog" />
+        <Mymodal />
         <div className="row mt-5">
-          <Sidebar items={data}/>
-          <Content />
+          <Content articles = {articles} />
+          <div className="col-4">
+            <Categories items = {categories} title = "Categories" />
+            <Categories items = {archives} title = "Archives" />
+          </div>
         </div>
       </Container>
     )
   }
 }
-
-
 
 ReactDom.render(<Layout />, document.getElementById('root'));
