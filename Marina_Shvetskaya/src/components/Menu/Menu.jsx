f6926313e9c4dbb5e62664b850370d0219d9b90a
missisol <missisololol@gmail.com>
2018-11-08 13:40:13 +0300

    Directory "Menu" updated

--- Marina_Shvetskaya/src/components/Menu/Menu.jsx
+++ Marina_Shvetskaya/src/components/Menu/Menu.jsx
@@ -1,24 +1,53 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
+} from 'reactstrap';
 
 export default class Menu extends Component {
   static propTypes = {
-    title: PropTypes.string.isRequired,
+    titleBlog: PropTypes.string.isRequired,
     items: PropTypes.arrayOf(
       PropTypes.shape({
         title: PropTypes.string.isRequired,
         href: PropTypes.string.isRequired,
       })
     )
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isOpen: false
+    };
   }
 
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  };
+
   render() {
-    const {title, items} = this.props;
+    const {items, titleBlog} = this.props;
     return (
       <Fragment>
-         <ul className="nav nav-tabs mb-3">
-          {items.map((item) => <li className="nav-item"><a className="nav-link" href={item.href}>{item.title}</a></li>)}
-        </ul>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">{titleBlog}</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              {items.map((item, idx) => <NavItem key={idx}><NavLink href={item.href}>{item.title}</NavLink></NavItem>)}
+            </Nav>
+          </Collapse>
+        </Navbar>
       </Fragment>
     )
   }
