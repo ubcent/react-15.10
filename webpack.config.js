6ea33cebadad700a7b09f7a3a4e74045c728cd71
Impress <impress@yandex.ru>
2018-11-12 21:32:29 +0300

    resolving the conflict

--- webpack.config.js
+++ webpack.config.js
@@ -3,38 +3,39 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: {
-        main: path.resolve(__dirname, 'src', 'index.js'),
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract ({
-                    fallback: 'style-loader',
-                    use: ['css-loader']
-                })
-            }
-        ]
-    },
-    plugins: [
-        new ExtractTextPlugin({
-            filename: 'style.css'
-        }),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'src', 'index.html'),
-            filename: 'index.html'
+  entry: {
+    main: path.resolve(__dirname, 'src', 'index.jsx'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader'],
         })
+      }
     ]
+  },
+  plugins: [
+    new ExtractTextPlugin({ filename: 'style.css' }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+      filename: 'index.html',
+    })
+  ]
 }
