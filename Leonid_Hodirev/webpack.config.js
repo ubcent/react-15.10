30f1f9d3880eb8e93c9bc763f9896974aa8423be
Leon <leonhodirev@mail.ru>
2018-11-15 22:24:03 +0300

    Урок 2. Настройка среды разработки: дополнение

--- Leonid_Hodirev/webpack.config.js
+++ Leonid_Hodirev/webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
-        main: path.resolve(__dirname, 'src', 'js', 'index.js')
+        main: path.resolve(__dirname, 'src', 'index.jsx')
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -13,7 +13,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: /\.jsx?$/,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader'
